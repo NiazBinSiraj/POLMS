3b1b88a8acc3fd4800343ce939750d46d0539300
Niaz Bin Siraj <niazbinsiraj@tekhomelabs.com>
2021-06-12 02:54:18 +0600

    login-page view completed

--- POLMS-Frontend/src/app/app.module.ts
+++ POLMS-Frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NavBarComponent } from './root-components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
